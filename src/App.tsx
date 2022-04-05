3c72fc2feffe0bca664bb9d3dc0cf63d53af2b96
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-05 22:53:27 +0900

    feat: 基本的な遷移設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 import './App.css'
+import PostIndex from './pages/posts/PostIndex'
+import UserIndex from './pages/users/UserIndex'
+// import UserShow from './pages/users/UserShow'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>アウトプット用のプロジェクト</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">TOPページ</Link>
+        <br />
+        <Link to="/users">ユーザー一覧</Link>
+        <br />
+        <Link to="/posts">ポスト一覧</Link>
+      </div>
+      <Switch>
+        <Route exact path="/users">
+          <UserIndex />
+        </Route>
+        <Route exact path="/posts">
+          <PostIndex />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
